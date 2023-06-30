56341953f0181ce58288ad2f34f78eaae50a4279
ErikHeller <erik.m.heller@gmail.com>
2023-06-30 15:59:18 -0400

    Preprocess depmap expression data.

--- preprocess_expression.R
+++ preprocess_expression.R
@@ -22,10 +22,12 @@ dir.create(output_data_dir, recursive = TRUE)
 dir.create(plots_dir, recursive = TRUE)
 
 # === Load Datasets ===
-## Load PhosphoSitePlus datasets
+## Load dataset from Goncalves et al. (DOI: https://doi.org/10.6084/m9.figshare.19345397.v1)
 procan_expr_avg <- read_excel(here(expression_data_dir, "ProCan-DepMapSanger_protein_matrix_6692_averaged.xlsx"))
+## Load Proteomics dataset from DepMap
+depmap_expr <- read_csv_arrow(here(expression_data_dir, "Broad-DepMap-Proteomics.csv"))
 
-# === Tidy Dataset ===
+# === Tidy Datasets ===
 
 procan_expr_avg_tidy <- procan_expr_avg %>%
   pivot_longer(everything() & !Project_Identifier,
@@ -39,7 +41,23 @@ procan_expr_avg_tidy <- procan_expr_avg %>%
   unite("UniqueId", c("CellLine.SangerModelId", "Protein.Uniprot.Accession"),
         sep = '_', remove = FALSE)
 
-# === Preprocess Dataset ===
+depmap_expr_tidy <- depmap_expr %>%
+  rename(CellLine.DepMapModelId = 1) %>%
+  pivot_longer(everything() & !CellLine.DepMapModelId,
+               names_to = "Protein", values_to = "Protein.Expression.Log2") %>%
+  # ToDo: Find more elegant way
+  separate_wider_delim(Protein,
+           delim = " ",
+           names = c("Gene.Symbol", "Protein.Uniprot.Accession")) %>%
+  mutate(Protein.Uniprot.Accession = gsub("[()]", "", Protein.Uniprot.Accession)) %>%
+  unite("UniqueId", c("CellLine.DepMapModelId", "Gene.Symbol", "Protein.Uniprot.Accession"),
+        sep = '_', remove = FALSE) %>%
+  # This requirement is odd
+  unite("UniqueProtId", c("Gene.Symbol", "Protein.Uniprot.Accession"),
+        sep = '_', remove = FALSE)
+
+
+# === Preprocess Datasets ===
 
 normalize_celllines <- function(df, cellline_col, value_col, group_col, normalized_colname = "Normalized") {
   df_pre <- data.frame(df)
@@ -78,21 +96,39 @@ procan_expr_avg_processed <- procan_expr_avg_tidy %>%
   mapIds("UNIPROT", "SYMBOL",
          "Protein.Uniprot.Accession", "Gene.Symbol")
 
+depmap_expr_processed <- depmap_expr_tidy %>%
+  remove_noisefloor(Protein.Expression.Log2) %>%
+  normalize_celllines(CellLine.DepMapModelId, Protein.Expression.Log2, UniqueProtId,
+                      normalized_colname = "Protein.Expression.Normalized") %>%
+  select(-UniqueProtId)
+
 
 # === Quality Control ===
 
-expr_dist <- procan_expr_avg_processed %>%
+procan_expr_dist <- procan_expr_avg_processed %>%
   ggplot() +
   geom_density(na.rm = TRUE, aes(Protein.Expression.Log2, color = "Non-Normalized"), show.legend = TRUE) +
   geom_density(na.rm = TRUE, aes(Protein.Expression.Normalized, color = "Normalized"), show.legend = TRUE) +
   xlab("Protein Expression")
 
-ggsave(here(plots_dir, "expression_distributions.png"), plot = expr_dist,
+ggsave(here(plots_dir, "expression_distributions_goncalves.png"), plot = procan_expr_dist,
+       height = 200, width = 300, units = "mm", dpi = 300)
+
+depmap_expr_dist <- depmap_expr_processed %>%
+  ggplot() +
+  geom_density(na.rm = TRUE, aes(Protein.Expression.Log2, color = "Non-Normalized"), show.legend = TRUE) +
+  geom_density(na.rm = TRUE, aes(Protein.Expression.Normalized, color = "Normalized"), show.legend = TRUE) +
+  xlab("Protein Expression")
+
+ggsave(here(plots_dir, "expression_distributions_depmap.png"), plot = depmap_expr_dist,
        height = 200, width = 300, units = "mm", dpi = 300)
 
 ## ToDo: Create & Write report
 
 # === Write Processed dataset to disk ===
 
-write_parquet(procan_expr_avg_processed, here(output_data_dir, 'expression_average.parquet'),
+write_parquet(procan_expr_avg_processed, here(output_data_dir, 'expression_average_goncalves.parquet'),
+              version = "2.6")
+
+write_parquet(depmap_expr_processed, here(output_data_dir, 'expression_depmap.parquet'),
               version = "2.6")
