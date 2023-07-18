9ee112844dc5775fce8c2de36e51b49a26ac05c9
ErikHeller <erik.m.heller@gmail.com>
2023-07-18 16:19:25 -0400

    Refactor cell line analysis and apply analysis to both datasets.

--- Code/02_Analysis/cellline_analysis.R
+++ Code/02_Analysis/cellline_analysis.R
@@ -27,46 +27,69 @@ dir.create(plots_dir, recursive = TRUE)
 # === Load Datasets ===
 
 expr_buf_goncalves <- read_parquet(here(output_data_dir, "expression_buffering_goncalves.parquet"))
+expr_buf_depmap <- read_parquet(here(output_data_dir, "expression_buffering_depmap.parquet"))
 
 # === Analyze Dosage Compensation on Cell Line level ===
 
-mean_pop <- mean(expr_buf_goncalves$Buffering.GeneLevel.Ratio, na.rm = TRUE)
-sd_pop <- sd(expr_buf_goncalves$Buffering.GeneLevel.Ratio, na.rm = TRUE)
+analyze_cellline_buffering <- function(df, buffering_ratio_col) {
+  mean_pop <- mean(df[[quo_name(enquo(buffering_ratio_col))]], na.rm = TRUE)
+  sd_pop <- sd(df[[quo_name(enquo(buffering_ratio_col))]], na.rm = TRUE)
 
-# ToDo: Why are there only 616 cell lines?
-cellline_buf_avg <- expr_buf_goncalves %>%
-  select(UniqueId, starts_with("CellLine"), Buffering.GeneLevel.Ratio) %>%
-  group_by(CellLine.Name) %>%
-  summarize(Buffering.CellLine.Ratio = mean(Buffering.GeneLevel.Ratio, na.rm = TRUE)) %>%
-  mutate(Buffering.CellLine.Ratio.ZScore = (Buffering.CellLine.Ratio - mean_pop) / sd_pop,
-         Rank = as.integer(rank(Buffering.CellLine.Ratio.ZScore))) %>%
-  arrange(Rank)
+  cellline_buf_avg <- df %>%
+    select(UniqueId, starts_with("CellLine"), { { buffering_ratio_col } }) %>%
+    group_by(CellLine.Name) %>%
+    summarize(Buffering.CellLine.Ratio = mean({ { buffering_ratio_col } }, na.rm = TRUE)) %>%
+    mutate(Buffering.CellLine.Ratio.ZScore = (Buffering.CellLine.Ratio - mean_pop) / sd_pop,
+           Rank = as.integer(rank(Buffering.CellLine.Ratio.ZScore))) %>%
+    arrange(Rank)
 
-## Write table
-write.xlsx(cellline_buf_avg, here(tables_base_dir, "cellline_dosage_compensation.xlsx"),
+  return(cellline_buf_avg)
+}
+
+waterfall_plot <- function(df, value_col, rank_col, label_col) {
+  xlim <- c(0, max(df[[quo_name(enquo(rank_col))]]))
+  label_nudge_x <- floor(xlim[2] / 5)
+
+  ylim1 <- c(min(df[[quo_name(enquo(value_col))]]),
+             (df %>% filter({ { rank_col } } == label_nudge_x))[[quo_name(enquo(value_col))]])
+  ylim2 <- c((df %>% filter({ { rank_col } } == xlim[2] - label_nudge_x))[[quo_name(enquo(value_col))]],
+             max(df[[quo_name(enquo(value_col))]]))
+
+  df %>%
+    ggplot() +
+    aes(x = { { rank_col } }, y = { { value_col } }, label = { { label_col } }) +
+    geom_hline(yintercept = 0, color = "red") +
+    geom_point(size = 0.3) +
+    geom_text_repel(data = df %>% slice_min({ { rank_col } }, n = 5),
+                    xlim = xlim, ylim = ylim1, direction = "y", nudge_x = label_nudge_x,
+                    seed = 42, color = "darkblue") +
+    geom_text_repel(data = df %>% slice_max({ { rank_col } }, n = 5),
+                    xlim = xlim, ylim = ylim2, direction = "y", nudge_x = -label_nudge_x,
+                    seed = 42, color = "darkred")
+}
+
+cellline_buf_goncalves <- expr_buf_goncalves %>%
+  analyze_cellline_buffering(Buffering.GeneLevel.Ratio)
+
+write.xlsx(cellline_buf_goncalves, here(tables_base_dir, "cellline_buffering_goncalves.xlsx"),
            colNames = TRUE)
 
+cellline_buf_waterfall_goncalves <- cellline_buf_goncalves %>%
+  waterfall_plot(Buffering.CellLine.Ratio.ZScore, Rank, CellLine.Name)
 
-## Waterfall plot
-xlim <- c(0, max(cellline_buf_avg$Rank))
-label_nudge_x <- floor(xlim[2] / 5)
+ggsave(here(plots_dir, "cellline_buffering_waterfall_goncalves.png"),
+       plot = cellline_buf_waterfall_goncalves,
+       height = 200, width = 200, units = "mm", dpi = 300)
 
-ylim1 <- c(min(cellline_buf_avg$Buffering.CellLine.Ratio.ZScore),
-           (cellline_buf_avg %>% filter(Rank == label_nudge_x))[["Buffering.CellLine.Ratio.ZScore"]])
-ylim2 <- c((cellline_buf_avg %>% filter(Rank == xlim[2] - label_nudge_x))[["Buffering.CellLine.Ratio.ZScore"]],
-           max(cellline_buf_avg$Buffering.CellLine.Ratio.ZScore))
+cellline_buf_depmap <- expr_buf_depmap %>%
+  analyze_cellline_buffering(Buffering.GeneLevel.Ratio)
 
-waterfall_plot <- cellline_buf_avg %>%
-  ggplot() +
-  aes(x = Rank, y = Buffering.CellLine.Ratio.ZScore, label = CellLine.Name) +
-  geom_hline(yintercept = 0, color ="red") +
-  geom_point(size = 0.3) +
-  geom_text_repel(data = cellline_buf_avg %>% slice_min(Rank, n = 5),
-                  xlim = xlim, ylim = ylim1, direction = "y", nudge_x = label_nudge_x,
-                  seed = 42, color = "darkblue") +
-  geom_text_repel(data = cellline_buf_avg %>% slice_max(Rank, n = 5),
-                  xlim = xlim, ylim = ylim2, direction = "y", nudge_x = -label_nudge_x,
-                  seed = 42, color = "darkred")
-ggsave(here(plots_dir, "cellline_buffering_waterfall.png"),
-       plot = waterfall_plot,
+write.xlsx(cellline_buf_depmap, here(tables_base_dir, "cellline_buffering_depmap.xlsx"),
+           colNames = TRUE)
+
+cellline_buf_waterfall_depmap <- cellline_buf_depmap %>%
+  waterfall_plot(Buffering.CellLine.Ratio.ZScore, Rank, CellLine.Name)
+
+ggsave(here(plots_dir, "cellline_buffering_waterfall_depmap.png"),
+       plot = cellline_buf_waterfall_depmap,
        height = 200, width = 200, units = "mm", dpi = 300)
